d0e8ba253610663861ba4ad3db8607fcba206e50
MONIGIR <e.urias24@info.uas.edu.mx>
2026-05-26 11:30:15 -0700

    CONEXION A POSTGRE, REGISTRO E INSTALACION DE LIBRERIAS

--- server.js
+++ server.js
@@ -13,6 +13,31 @@ console.log(`
     `);
 
 const {Client} = require('pg');
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4000;
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs'); // Configura EJS como el motor de plantillas
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.render('index'); //para renderizar el archivo Index.ejs
+});
+
+app.get('/users/registro', (req, res) => {
+    res.render('registro'); //para renderizar el archivo registro.ejs
+});
+
+app.get('/users/login', (req, res) => {
+    res.render('indexLogin'); //para renderizar el archivo indexLogin.ejs
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
 
 const con= new Client({
     host: 'localhost',
