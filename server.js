c1acf55faa34bc21f004fcb175309872144e29a2
MONIGIR <e.urias24@info.uas.edu.mx>
2026-05-31 10:34:55 -0700

    Add user registration and dashboard features, update dependencies

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ console.log(`
 const {Client} = require('pg');
 const express = require('express');
 const app = express();
+const { pool } = require('./dbConfig');
 const path = require('path');
 const PORT = process.env.PORT || 4000;
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs'); // Configura EJS como el motor de plantillas
-app.use(express.static(path.join(__dirname)));
+app.use(express.urlencoded({ extended: false })); // Middleware para parsear el cuerpo de las solicitudes
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index'); //para renderizar el archivo Index.ejs
@@ -35,6 +37,33 @@ app.get('/users/login', (req, res) => {
     res.render('indexLogin'); //para renderizar el archivo indexLogin.ejs
 });
 
+app.get('/users/dashboard', (req, res) => {
+    res.render('dashboard'); //para renderizar el archivo dashboard.ejs
+});
+
+app.post('/users/registro', async (req, res) => {
+   let { nombre, correo, password_hash } = req.body;
+   console.log(`Datos recibidos: Name=${nombre}, Email=${correo}, Password=${password_hash}`);
+
+   let errors = [];
+
+   if (!nombre || !correo || !password_hash || !password_hash2 ) {
+       errors.push('Por favor, complete todos los campos');
+   }
+
+   if (password_hash !== password_hash2) {
+       errors.push('Las contraseñas no coinciden');
+
+    if (password_hash.length < 15) {
+        errors.push('La contraseña debe tener al menos 15 caracteres');
+    }
+    
+   }
+
+   if (errors.length > 0) {
+       return res.render('registro', { errors });
+   }
+});
 app.listen(PORT, () => {
     console.log(`Servidor Express escuchando en el puerto ${PORT}`);
 });
